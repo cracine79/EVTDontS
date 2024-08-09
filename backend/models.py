448027ab979d58a0207d268f8168fb991fa254da
CharLee Racine <charleeracine@yahoo.com>
2024-08-08 22:08:13 -0600

    create database

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,10 @@
 from exts import db
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, text
 
+class Base(DeclarativeBase):
+    pass
 
 """
     class: User
@@ -9,10 +14,10 @@ from exts import db
 """
 
 class User(db.Model):
-    id=db.Column(db.Integer, primary_key=-True)
-    username=db.Column(db.String(25), nullable=False, unique=True)
-    email=db.Column(db.String(80), nullable=False) 
-    password_hash=db.Column(db.Text(),nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    email: Mapped[str]
+    password_hash: Mapped[str]
 
     def __repr__(self):
         return f"User <{self.username}>"
