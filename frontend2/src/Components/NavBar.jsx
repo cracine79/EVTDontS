ed12a0787f5e656760aab535f829226509448ad4
CharLee Racine <charleeracine@yahoo.com>
2024-08-22 10:39:42 -0600

    update title

--- frontend2/src/Components/NavBar.jsx
+++ frontend2/src/Components/NavBar.jsx
@@ -2,8 +2,8 @@ export const NavBar = () => {
     return(
         <>
         
-            <nav className="w-full h-7 bg-black">
-
+            <nav className="w-full h-20 bg-lime-500">
+                <div>Economics Videos That Don't Suck</div>
             </nav>
         </>
     )
