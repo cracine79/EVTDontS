e026a801e626aecb1eab17538961ed3c785113cc
CharLee Racine <charleeracine@yahoo.com>
2024-08-23 15:56:50 -0600

    working on modal

--- frontend2/src/Components/NavBar.jsx
+++ frontend2/src/Components/NavBar.jsx
@@ -1,30 +1,41 @@
 import { useSelector } from "react-redux"
+import { useState } from "react"
+import LoginComponent from "./LoginComponent"
 
 
 
 export const NavBar = () => {
+    const [loginFormOpen, setLoginFormOpen] = useState(false)
     const currentUser = useSelector(state=>(state.user.username))
     console.log(currentUser)
     const handleLogin = () =>{
-
+        setLoginFormOpen(true)
     }
 
     const sessionLinks = currentUser ? (
         <>Signed In Already</>
     ) : (
-        <>
-            <p>Sign Up</p>
-            <p onClick = {handleLogin} >Log In Here</p>
-        </>
+        <div className="flex w-32 justify-around mr-10">
+            <p className="hover:cursor-pointer">Sign Up</p>
+            <p className="hover:cursor-pointer" onClick = {handleLogin} >Log In</p>
+        </div>
     )
     return(
         <>
         
-            <nav className="w-full h-24 bg-lime-500 items-center flex">
-                
+            <nav className=
+                "w-full 
+                h-24 
+                bg-lime-500 
+                items-center 
+                flex justify-between">
                 <img src="/Logo.svg" alt="EVTDS Logo" className="h-3/4 ml-2.5"/>
-                {sessionLinks}
+                <div>
+                    {sessionLinks}
+                </div>
             </nav>
+            {loginFormOpen && (<LoginComponent />)}
+            
         </>
     )
 }
