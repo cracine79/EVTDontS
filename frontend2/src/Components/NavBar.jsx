7bae0807447fd751e728417a47e976cbfa8b5bcb
CharLee Racine <charleeracine@yahoo.com>
2024-08-22 21:32:22 -0600

    working on nav bar

--- frontend2/src/Components/NavBar.jsx
+++ frontend2/src/Components/NavBar.jsx
@@ -1,10 +1,26 @@
+import { useSelector } from "react-redux"
+
+
+
 export const NavBar = () => {
+    const currentUser = useSelector(state=>(state.user.username))
+    console.log(currentUser)
+
+    const sessionLinks = currentUser ? (
+        <>Signed In Already</>
+    ) : (
+        <>
+            <p>Sign Up</p>
+            <p>Log In</p>
+        </>
+    )
     return(
         <>
         
-            <nav className="w-full h-20 bg-lime-500">
-                Hello
-                <img src="/Logo.svg" alt="Logo" />
+            <nav className="w-full h-24 bg-lime-500 items-center flex">
+                
+                <img src="/Logo.svg" alt="Logo" className="h-3/4 ml-2.5"/>
+                {sessionLinks}
             </nav>
         </>
     )
