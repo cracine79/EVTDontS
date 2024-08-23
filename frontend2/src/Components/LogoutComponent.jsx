781133bdef51b78b57c69b491cb0be99f9798f29
CharLee Racine <charleeracine@yahoo.com>
2024-08-23 16:24:56 -0600

    cleanup

--- frontend2/src/Components/LogoutComponent.jsx
+++ frontend2/src/Components/LogoutComponent.jsx
@@ -1,29 +1,22 @@
 import { useDispatch } from 'react-redux';
 import { logout } from '../Slices/userSlice';
-import { csrfFetch } from '../csrf';
+
 
 const LogoutButton = () => {
   const dispatch = useDispatch();
 
   const handleLogout = async () => {
-    // await csrfFetch('/api/auth/logout', {
-    //   method: 'POST',
-    // });
     localStorage.setItem('access_token', null);
     dispatch(logout()); // Clear user from Redux state
   };
 
-  const test = async () => {
-    const res = await csrfFetch('/api/questions/hello')
-    const better=await res.json()
-    console.log(better)
-  }
+
 
   return (
     <>
-      {/* "Logout Button" */}
+
         <button onClick={handleLogout}>Logout</button>
-        {/* <button onClick={test}>Test</button> */}
+   
     </>
   )
 };
