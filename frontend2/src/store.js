840ee8ed36b0a9156ba6fa596049a8f40230a0a2
CharLee Racine <charleeracine@yahoo.com>
2024-08-24 10:28:45 -0600

    add login signup modals in state

--- frontend2/src/store.js
+++ frontend2/src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './Slices/userSlice';
+import modalReducer from './Slices/modalSlice'
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    modal: modalReducer
   },
 });
