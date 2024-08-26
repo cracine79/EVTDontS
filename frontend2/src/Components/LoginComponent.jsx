61cafa1b290cbfb31924667df787a2df95360f02
CharLee Racine <charleeracine@yahoo.com>
2024-08-25 20:11:39 -0600

    login

--- frontend2/src/Components/LoginComponent.jsx
+++ frontend2/src/Components/LoginComponent.jsx
@@ -25,7 +25,7 @@ const LoginComponent = () => {
       // Log the status and response text for debugging
       const errorText = await response.text();
       console.error('Error response status:', response.status);
-      console.error('Error response body:', errorText);
+      console.error('Error response body: ', errorText);
       // Optionally, throw an error to handle it later in the catch block
       throw new Error(`Error ${response.status}: ${errorText}`);
     }
