ddd87f5440330699db7105633bd98ed34a20f318
CharLee Racine <charleeracine@yahoo.com>
2024-08-27 16:08:54 -0600

    vid added

--- frontend2/src/Components/MainPage.jsx
+++ frontend2/src/Components/MainPage.jsx
@@ -3,7 +3,14 @@ import LogoutButton from "./LogoutComponent"
 export const MainPage = () => {
     return(
         <>
-            Hello from MainPage
+            <div className="mt-20 ml-20 flex w-11/12">
+                <div className="font-serif text-5xl w-1/2 ">
+                    Learning economics doesn't have to suck...   But it usually does. 
+                </div>
+                <video className="w-1/2" autoPlay muted>
+                    <source src="Intro.mp4" type="video/mp4" />
+                </video>
+            </div>
         </>
     )
 }
