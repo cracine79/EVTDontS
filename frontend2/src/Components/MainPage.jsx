53bd862cac92a135d68df6ea161e4588afc9d9e1
CharLee Racine <charleeracine@yahoo.com>
2024-08-27 16:14:19 -0600

    resize splash page vid

--- frontend2/src/Components/MainPage.jsx
+++ frontend2/src/Components/MainPage.jsx
@@ -3,11 +3,11 @@ import LogoutButton from "./LogoutComponent"
 export const MainPage = () => {
     return(
         <>
-            <div className="mt-20 ml-20 flex w-11/12">
-                <div className="font-serif text-5xl w-1/2 ">
+            <div className="flex ">
+                {/* <div className="font-serif text-5xl w-1/2 ">
                     Learning economics doesn't have to suck...   But it usually does. 
-                </div>
-                <video className="w-1/2" autoPlay muted>
+                </div> */}
+                <video autoPlay loop muted>
                     <source src="Intro.mp4" type="video/mp4" />
                 </video>
             </div>
