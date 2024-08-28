2ab4c83a8478e37c3017815c3680ac09523f58b7
CharLee Racine <charleeracine@yahoo.com>
2024-08-28 10:43:06 -0600

    return to main with logout

--- frontend2/src/Components/LogoutComponent.jsx
+++ frontend2/src/Components/LogoutComponent.jsx
@@ -1,13 +1,15 @@
 import { useDispatch } from 'react-redux';
 import { logout } from '../Slices/userSlice';
-
+import { useNavigate } from 'react-router-dom';
 
 const LogoutButton = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const handleLogout = async () => {
     localStorage.setItem('access_token', null);
     dispatch(logout()); // Clear user from Redux state
+    navigate('/')
   };
 
 
