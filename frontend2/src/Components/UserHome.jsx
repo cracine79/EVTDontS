7759a04685348ab019dc172fe2506646eb582f46
CharLee Racine <charleeracine@yahoo.com>
2024-09-10 22:20:03 -0600

    userHome styling

--- frontend2/src/Components/UserHome.jsx
+++ frontend2/src/Components/UserHome.jsx
@@ -14,7 +14,7 @@ export const UserHome = () => {
         "Glad to see you're back.  We were starting to think you'd mastered economics overnight",
         "Already back for more?  Guess Netflix isn't cutting it today",
         "Here again?, we knew you couldn't resist the allure of more charts and graphs",
-        "We're starting to think you actually enjoy this stuff.",
+        "We're starting to think you actually enjoy this sh... this stuff.",
         "Back for more?  We promise not to make this TOO interesting.",
         "We've got to stop meeting like this!",
         "We've never felt so loved before.  Except the time our Mom subscribed to our Youtube channel",
@@ -92,6 +92,7 @@ export const UserHome = () => {
 
     
     return(
+    
         <div className=
             'flex 
             items-center 
@@ -100,40 +101,46 @@ export const UserHome = () => {
             bg-slate-50 
             h-screen
             flex-col'>
-            <div className=
-                'mt-36 
-                w-3/4 
-                h-1/4 
-                bg-white 
-                rounded-3xl 
-                shadow-2xl 
-                flex 
-                flex-row 
-                items-center
-                justify-center'>
-               {userName ? (
-                        <div className='flex flex-col ml-10 items-center justify-center'>
-                            <div className='ml-10 text-5xl'>
-                                Welcome Back {userName[0].toUpperCase() + userName.slice(1)}.
+            <div className='h-full flex flex-row mt-20'>
+                <div className=
+                    'mt-36 
+                    w-3/5 
+                    h-2/5 
+                    bg-white 
+                    rounded-3xl 
+                    shadow-2xl 
+                    flex 
+                    flex-row 
+                    items-center
+                    justify-center'>
+                {userName ? (
+                            <div className='flex flex-col items-center justify-center'>
+                                <div className='ml-10 text-5xl'>
+                                    Welcome Back {userName[0].toUpperCase() + userName.slice(1)}.
+                                </div>
+                                <div className='text-xl w-4/5 text-center mt-6'>
+                                    {genMessage()}
+                                </div>
                             </div>
-                            <div className='text-xl w-4/5 text-center mt-6'>
-                                {genMessage()}
-                            </div>
+                
+                    ) : (
+                        <p className='ml-10 text-5xl'>
+                        </p>
+                    )}
+                </div>
+                <div className="w-1/6 bg-white 100 h-3/5 mt-20 ml-12 rounded-3xl shadow-2xl">
+                        <div className = "mt-8">
+
+                            {workingOn()}
                         </div>
-             
-                ) : (
-                    <p className='ml-10 text-5xl'>
-                    </p>
-                )}
+                        <p>Up Next</p>
+                        <div className='mt-10' onClick={handleClick}>{upNext()}</div>
+                    </div>
             </div>
             <div className='flex flex-com justify-center w-screen'>
                 <div className='flex flex-row items-center justify-center w-11/12'>
                     <Progress />
-                    <div className="w-1/6 bg-white 100 h-3/5 mt-20 ml-12 rounded-3xl shadow-2xl">
-                        {workingOn()}
-                        <p>Up Next</p>
-                        <div className='mt-10' onClick={handleClick}>{upNext()}</div>
-                    </div>
+
                 </div>
             </div>
         </div>
