0035844b986df4f1cde13d1ddc4ebb7d011402f8
CharLee Racine <charleeracine@yahoo.com>
2024-09-13 10:40:49 -0600

    add back to dashboard button on quiz page

--- frontend2/src/Components/Quiz.jsx
+++ frontend2/src/Components/Quiz.jsx
@@ -1,11 +1,13 @@
 import { useSelector, useDispatch } from "react-redux"
-import { useLocation } from "react-router-dom"
+import { useLocation, useNavigate } from "react-router-dom"
 import { getQuestions } from "../Slices/questionsActions"
 import { useEffect } from "react"
 import { QuestionComponent } from "./QuestionComponent"
 import { openQuizModal } from "../Slices/modalSlice"
 
+
 export const Quiz = () => {
+    const navigate = useNavigate()
     const dispatch = useDispatch()
     const location = useLocation()
     const chapter = location.state?.chapter
@@ -18,12 +20,16 @@ export const Quiz = () => {
     const handleOpen = () => {
         dispatch(openQuizModal())
     }
+    const goHome = ()=>{
+        navigate('/userhome')
+    }
 
     return (
         <div className="w-full flex flex-col justify-center items-center">
             <div className='mt-40  aspect-video justify-center w-3/4'>
                 <div>This is the page for the quiz for Chapter {chapter}</div>
                 <button onClick={handleOpen}>Start The Quiz</button>
+                <button onClick={goHome}>Back to Dashboard</button>
             </div>
             <QuestionComponent />
         </div>
