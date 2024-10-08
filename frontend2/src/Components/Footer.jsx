6c6c9269757aef4e1f37ed3ea1389f0ec6cc72ed
CharLee Racine <charleeracine@yahoo.com>
2024-10-08 12:49:31 -0600

    add footer

--- frontend2/src/Components/Footer.jsx
+++ frontend2/src/Components/Footer.jsx
@@ -1,7 +1,12 @@
 export const Footer = () => {
     return (
         <>
-            <div></div>
+            <div className='h-32 bg-green-600 flex justify-end items-center'>
+                <div className="flex justify-end flex-col w-1/4">
+
+                <span className="text-white">All videos, images, and practice questions are property of EVTDS.com and may not be reproduced without express written consent.</span>
+                </div>
+            </div>
         </>
     )
 }
