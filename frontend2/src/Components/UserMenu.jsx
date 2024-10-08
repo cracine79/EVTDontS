951f08cdd8b4b90bb0b97f84ef71db134f8fec87
CharLee Racine <charleeracine@yahoo.com>
2024-10-07 22:43:07 -0600

    add logout go to next thing go to userhome to user menu next is review quiz page

--- frontend2/src/Components/UserMenu.jsx
+++ frontend2/src/Components/UserMenu.jsx
@@ -1,25 +1,54 @@
 import { FaUserCircle } from "react-icons/fa";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import LogoutButton from "./LogoutComponent";
-
-
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { clearQuestions } from "../Slices/questionsSlice"
+import { clearUserResults } from "../Slices/resultsSlice"
 
 export const UserMenu = () => {
     const username = useSelector((state) => state.user.username)
+    const capitalized = username[0].toUpperCase()+username.slice(1)
+    const currentChapterId = useSelector((state) => state.user.currentChapter)
+    const videoWatched = useSelector((state)=>state.chapters[currentChapterId].video_completed)
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    
+    const thing = videoWatched ? "chapter quiz" : "next video"
+    console.log(videoWatched)
+
+    const nextThing = () => {videoWatched ? 
+        navigate('/Quiz', {state: {chapter: currentChapterId, type: 'chapterQuiz', topics: []}}) :
+        navigate('/Video', {state: {chapter: currentChapterId}})
+    }
+
+    const goHome = () => {
+        dispatch(clearQuestions)
+        dispatch(clearUserResults)
+        navigate('/userhome')
+    }
+
     return(
             <div className='relative inline-block group'>
-                <div className='flex flex-col items-center mr-10 mt-2 hover:cursor-pointer text-slate-600 '>
+                <div className='flex flex-col items-center mr-6 mt-2 hover:cursor-pointer text-slate-600 '>
                     <FaUserCircle className='text-3xl '/>
                     <div>Menu</div>
                 </div>
                 <div className=
-                    'h-36 w-52 shadow-lg  
+                    'h-auto w-54 shadow-lg  
                     border-slate-900 border 
                     border-solid bg-white 
-                    rounded-lg text-right px-6 pt-2 absolute -ml-32 flex flex-col hidden group-hover:block'>
-                    <div>{username}</div>
-                    <div className="hover:bg-slate-400 hover:bg-slate-400 w-52 -ml-6 px-6">Jump to next thing</div>
-                    <LogoutButton/>
+                    rounded-lg px-6 pt-2 absolute -ml-32 flex flex-col hidden group-hover:block'>
+                    <div className = 'font-bold text-lg'>{capitalized}</div>
+                    <hr className='border-gray-400 w-full my-2'></hr>
+                    <div className='text-right'>
+                        <div className="hover:bg-green-400 hover:cursor-pointer w-48 -ml-6 px-4" onClick = {nextThing}>Go to {thing}</div>
+                        <div className="hover:bg-green-400  hover:cursor-pointer w-48 -ml-6 px-4">Create review quiz</div>
+                        <div className="hover:bg-green-400  hover:cursor-pointer w-48 -ml-6 px-4" onClick = {goHome}>User dashboard</div>
+                        <div className="hover:bg-green-400  hover:cursor-pointer w-48 -ml-6 px-4 my-4">
+                            <LogoutButton/>
+                        </div>
+                    </div>
                 </div>
                 
             
