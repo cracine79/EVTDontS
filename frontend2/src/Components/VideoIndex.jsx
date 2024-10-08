17e33464f315243136a505835de0dd8c296fe1ea
CharLee Racine <charleeracine@yahoo.com>
2024-10-08 14:15:28 -0600

    play videos from menu selection

--- frontend2/src/Components/VideoIndex.jsx
+++ frontend2/src/Components/VideoIndex.jsx
@@ -1,10 +1,30 @@
+import { useState } from "react"
+import { useSelector } from "react-redux"
 export const VideoIndex = () => {
+    const chapters = useSelector(state=>(state.chapters))
+    const chaptersObj = Object.values(chapters)
+  
+    const [videoId, setVideoId] = useState(false)
+
+    const videoGo = (videoUrl) => {
+        setVideoId(videoUrl)
+    }
+
+    const chapterVid = (videoId) => {
+        const chapter = chaptersObj.find(chapter=>chapter.video_url === videoId)
+        return chapter.name
+    }
     return(
         <div className = 'mt-24'>
 
             <div className='h-screen w-100 flex'>
-                <div className='h-full w-1/6 bg-lime-100'>
+                <div className='h-full w-1/4 bg-lime-100'>
                     <div>Video Library</div>
+                    {chaptersObj.map(chapter=>{
+                        return(
+                            <div className='hover:cursor-pointer' onClick={()=>videoGo(chapter.video_url)}>{chapter.name}</div>
+                        )
+                    })}
                 </div>
                 <div className='w-5/6'>
                     <div className='text-center mt-10 text-5xl font-bold'>
@@ -16,8 +36,27 @@ export const VideoIndex = () => {
                     <div className='mx-8 mt-4 text-lg'>
                     Choose wisely, and don't be like the Nazi guy in the end of Indiana Jones & The Last Crusade!
                     </div>
+                    <div className='flex flex-col items-center justify-center mt-10 border'>
+                        <div>
+                            {videoId ? `Video for ${chapterVid(videoId)}`: 'nothing'}
+                        </div>
+                        {videoId && (
+                        <div>
+                            <iframe 
+                                width="920" 
+                                height="540" 
+                                src={videoId} 
+                                allowFullScreen>
+                            </iframe>
+                        </div>
+                        )}
+                    </div>
                 </div>
+
+            
+                    
             </div>
+        
         </div>
     )
 }
