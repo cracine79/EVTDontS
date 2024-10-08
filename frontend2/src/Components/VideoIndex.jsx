0524180a95529f5b349ee34eba336d3ccd99fbef
CharLee Racine <charleeracine@yahoo.com>
2024-10-08 13:37:40 -0600

    video cache formatting

--- frontend2/src/Components/VideoIndex.jsx
+++ frontend2/src/Components/VideoIndex.jsx
@@ -1,8 +1,23 @@
 export const VideoIndex = () => {
     return(
         <div className = 'mt-24'>
-            Hello from Video Index!
-            Choose a video from the menu to play in the player below
+
+            <div className='h-screen w-100 flex'>
+                <div className='h-full w-1/6 bg-lime-100'>
+                    <div>Video Library</div>
+                </div>
+                <div className='w-5/6'>
+                    <div className='text-center mt-10 text-5xl font-bold'>
+                    Welcome to the Video Cache!
+                    </div>
+                    <div className='mx-8 mt-10 text-lg'>
+                        You can access any and all of our treasure trove of videos that absolutely don't suck from the navigation bar on the left. 
+                    </div>
+                    <div className='mx-8 mt-4 text-lg'>
+                    Choose wisely, and don't be like the Nazi guy in the end of Indiana Jones & The Last Crusade!
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
