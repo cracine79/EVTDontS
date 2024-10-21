6a99895981cb1e42bbf48c613a8aff3198bc2936
CharLee Racine <charleeracine@yahoo.com>
2024-10-21 11:26:38 -0600

    add intro to user menu to orientation

--- backend/run.py
+++ backend/run.py
@@ -1,8 +1,6 @@
 from main import create_app
 from config import DevConfig
 
-print("hello")
-
 if __name__=='__main__':
     app=create_app(DevConfig)
     app.run(host="0.0.0.0", port=3000)
--- frontend2/src/Components/Tutorial.jsx
+++ frontend2/src/Components/Tutorial.jsx
@@ -4,10 +4,10 @@ export const Tutorial = ({showModal}) => {
 
     useEffect(() => {
         const hasVisited = localStorage.getItem('hasVisitedDashboard');
-        // if (showModal) {
-          setModalState(['on', 3]); // Show the modal
+        if (showModal) {
+          setModalState(['on', 0]); // Show the modal
           localStorage.setItem('hasVisitedDashboard', 'true'); // Set the key
-        // }
+        }
       }, []);
 
       useEffect(() => {
@@ -45,7 +45,36 @@ export const Tutorial = ({showModal}) => {
                                 </div>
                             </div>
             </>}
+
             {modalState[1]==1 && <>
+                <div
+                className="fixed inset-0 bg-black bg-opacity-60 z-40"
+                style={{
+                    clipPath: `polygon(
+                    0 0, 
+                    100% 0, 
+                    100% 100%, 
+                    0 100%, 
+                    0 0, /* Move these percentages to change the cutout */
+                    85% 0, 
+                    85% 300px, 
+                    100% 300px, 
+                    100% 0, 
+                    0 0
+                    )`, 
+                }}
+                >
+                </div>
+                <div className="absolute top-10 mt-12 left-1/2 z-50 bg-slate-100 z-100  p-4 rounded-md shadow-lg w-1/4 min-w-80">
+                                <p className='mb-4'>You can teleport out of this Econo-wasteland to any other part of the site by accessing your user menu.  From here you can go straight to your next task, access all of our captivating videos in one place </p>
+                                <p className='mb-4'>You can also visit our quiz generator, which will allow you to hand-craft a quiz by selecting topics</p>
+                                <div className='flex justify-around flex-row'>
+                                    <button className="bg-blue-500 px-4 py-2 rounded-md text-white hover:bg-blue-700" onClick={()=>setModalState(['on', 2])}>Amazing! More!</button>
+                                    <button className="bg-blue-500 px-4 py-2 rounded-md text-white hover:bg-blue-700"  onClick={()=>setModalState(['off', 1])}>Boring.  No more. </button>
+                                </div>
+                            </div>
+            </>}
+            {modalState[1]==2 && <>
                 <div
                 className="fixed inset-0 bg-black bg-opacity-60 z-40"
                 style={{
@@ -67,13 +96,13 @@ export const Tutorial = ({showModal}) => {
                 <div className="absolute top-10 mt-48 left-1/4 z-50 bg-slate-100 z-100  p-4 rounded-md shadow-lg w-1/4 min-w-80">
                                 <p className='mb-4'>Ah, the mighty Progress Box—your personal cheerleader, telling you what chapter you're currently <i>surviving</i>. Whether you're about to watch another mind-blowing video or dive into a quiz that you totally remember all the answers to, this button is your trusty guide. ust click it, and boom—on to your next step toward total mastery (or at least faking it till you make it)!"</p>
                                 <div className='flex justify-around flex-row'>
-                                    <button className="bg-blue-500 px-4 py-2 rounded-md text-white hover:bg-blue-700" onClick={()=>setModalState(['on', 2])}>Keep it the magic going!</button>
-                                    <button className="bg-blue-500 px-4 py-2 rounded-md text-white hover:bg-blue-700"  onClick={()=>setModalState(['off', 1])}>Eh, I'll wing it</button>
+                                    <button className="bg-blue-500 px-4 py-2 rounded-md text-white hover:bg-blue-700" onClick={()=>setModalState(['on', 3])}>Keep it the magic going!</button>
+                                    <button className="bg-blue-500 px-4 py-2 rounded-md text-white hover:bg-blue-700"  onClick={()=>setModalState(['off', 0])}>Eh, I'll wing it</button>
                                 </div>
                             </div>
             </>}
 
-            {modalState[1]==2 && <>
+            {modalState[1]==3 && <>
                 <div
                 className="fixed inset-0 bg-black bg-opacity-60 z-40"
                 style={{
@@ -96,13 +125,13 @@ export const Tutorial = ({showModal}) => {
                                 <p className='mb-4'>Welcome to your grand master plan... or as we like to call it, 'organized chaos.' Here’s where all your units live, along with a handy tracker to show exactly how much you’ve actually done. Spoiler: It's probably less than you'd like, but hey, progress is progress! </p>
                                 <p> Click on your first unit to expand it.</p>
                                 <div className='flex justify-around flex-row mt-4'>
-                                    <button className="bg-blue-500 px-4 py-2 rounded-md text-white hover:bg-blue-700" onClick={()=>setModalState(['on', 3])}>OK, I expanded it</button>
+                                    <button className="bg-blue-500 px-4 py-2 rounded-md text-white hover:bg-blue-700" onClick={()=>setModalState(['on', 4])}>OK, I expanded it</button>
                                     <button className="bg-blue-500 px-4 py-2 rounded-md text-white hover:bg-blue-700"  onClick={()=>setModalState(['off', 1])}>I don't feel like it.  I'm out.</button>
                                 </div>
                             </div>
             </>}
 
-            {modalState[1]==3 && <>
+            {modalState[1]==4 && <>
                 <div
                 className="fixed inset-0 bg-black bg-opacity-60 z-40"
                 style={{
