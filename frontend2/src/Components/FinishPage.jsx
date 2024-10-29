7d04163d17f85694cfe2bea4bd6b1044bafc6039
CharLee Racine <charleeracine@yahoo.com>
2024-10-29 08:30:07 -0600

    completion page text added

--- frontend2/src/Components/FinishPage.jsx
+++ frontend2/src/Components/FinishPage.jsx
@@ -1,5 +1,14 @@
 export const FinishPage = () => {
-    return(<div className='mt-36'>
-        Hello from finish page
+    return(<div className='mt-36 w-screen flex justify-center'>
+        <div className='w-4/5 mb-28 shadow-2xl rounded-3xl flex justify-cente flex-col items-center'>
+            
+            <p className='mt-16 text-3xl mb-4'>CONGRATULATIONS!!</p>
+            <p className='my-2 px-4'>Congratulations, you've officially crushed all the chapters you have enrolled in! 🎉 You watched all the videos, aced the quizzes, and now, my friend, you’re the economist everyone wishes they could hang out with at parties (…just go with it).</p>
+            <p className='my-2 px-4'>But wait, there’s more! If you’re craving more economic enlightenment (or looking for a distraction from another re-watch of <em>The Office</em>), there are still chapters you haven’t signed up for. Go ahead—dive back in, add those new ones, or do a power rewatch of your favorites.</p>
+            <p className='my-2 px-4'>And now, the moment you’ve been waiting for: your Mastery of Topics chart below! Yes, it’s like your very own stats in a video game—except instead of maxing out agility or charm, you've leveled up in things like Price Elasticity and Marginal Cost. Take a look, bask in the glory of your economic dominance, and feel free to screenshot and send it to that one friend who still thinks ‘opportunity cost’ is a new cryptocurrency.</p>
+            <p className='my-2 px-4'>And hey, if you’ve secretly enjoyed the thrilling world of supply and demand, don’t keep it to yourself. Tell a friend! They’ll thank you later...maybe. Or they'll pretend they didn't see your text. Either way, you are the econ star we all need. Shine on!</p>
+       
+            <p className='mt-8 text-2xl'> Mastery of Topics (Dun dun dunnnnnnn!)</p>
+       </div>
     </div>)
 }
