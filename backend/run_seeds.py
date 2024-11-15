34bca67fe7cfb58a11867d8f7016310ff57fcdd5
CharLee Racine <charleeracine@yahoo.com>
2024-11-14 22:52:04 -0700

    demand chapter questions seeded

--- backend/run_seeds.py
+++ backend/run_seeds.py
@@ -3,7 +3,8 @@ import subprocess
 def run_seeds():
     try:
         subprocess.run(['python', 'seed_db.py'], check=True)
-        subprocess.run(['python', 'seed_blurbs.py'])
+        subprocess.run(['python', 'seed_blurbs.py'], check=True)
+        subprocess.run(['python', 'seed_questions.py'], check=True)
     except subprocess.CalledProcessError as e:
         print(f"An error occurred: {e}")
 
--- backend/seed_questions.py
+++ backend/seed_questions.py
@@ -2,25 +2,120 @@
 from exts import db
 from main import create_app
 from config import ProdConfig
-from models import Chapter, QuestionTopic, Unit, Question
+from models import Chapter, QuestionTopic, Unit, Question, Answer
 from sqlalchemy.exc import IntegrityError
 
 app = create_app(ProdConfig)
 
 def seed_unit2_questions():
     print('Seeding Unit 2 Questions')
-    unit2 = Unit.query.get(2)
-    unit2_chapters = unit2.chapters
-    unit2_chapters_sorted = sorted(unit2_chapters, key=lambda chapter: chapter.order)
-    demand_curve_topic = unit2_chapters_sorted[0].topics
-    shifts_to_demand_topic = unit2_chapters_sorted[1].topics
+    
+    demandCurveTopic = QuestionTopic.query.get(10)
+    demandChapter = Chapter.query.get(8)
+    
 
-    question1 = Question(text="Which answer is correct?")
+    question1 = Question(text="Which of the following occurs as a result of the substitution effect of a decrease in the price of a normal good?", chapter=demandChapter, topic=demandCurveTopic)
+    question2 = Question(text = "The table above lists the monthly individual demahd schedules for pizza for the only three buyers in the market, April, Bob and Charlie.  Which of the following combinations of price and quantity lies on the demand curve?", chapter=demandChapter, topic=demandCurveTopic, image_url="https://evtds-seeds.s3.us-east-2.amazonaws.com/Screenshot+2024-11-14+at+8.14.41+PM.png")
+    question3 = Question(text="The graphs above show the individual demand curves for two consumers, Wes and Wyatt, in the market for salted peanuts.  As the price of salted peanuts decreases from $10 to $4, now does the quantity demanded change along the market demand curve?",  chapter=demandChapter, topic=demandCurveTopic, image_url="https://evtds-seeds.s3.us-east-2.amazonaws.com/WWDemands.png")
+    question4 = Question(text="For normal goods, the income effect of a price change refers to the change in the consumption of the good that takes place as a result of:", chapter=demandChapter, topic=demandCurveTopic)
+    question5 = Question(text="Which of the following can be used to explain why the demand curve for a normal good is downward sloping?", chapter=demandChapter, topic=demandCurveTopic)
+    question6 = Question(text="Which of the following most accurately defines the economic concept of demand?", chapter=demandChapter, topic=demandCurveTopic)
+    question7 = Question(text="Assume that pizza is a normal good.  If the price of pizza incerases, the substitution efefc and income effect will lead to which of the following changes in pizza consumption?", chapter=demandChapter, topic=demandCurveTopic)
+    question8 = Question(text="The above graphs show Frank's demand for hot dogs and Sam's demand for hot dogs.  Suppose Frank and Sam are the only two consumers in the market.  Which of the following is a point on the market demand curve for hot dogs?", chapter=demandChapter, topic=demandCurveTopic, image_url="https://evtds-seeds.s3.us-east-2.amazonaws.com/HDDemands.png")
+    question9 = Question(text="According to the law of demand, an increase in the price of peanut butter will result in", chapter=demandChapter, topic=demandCurveTopic)
+    question10 = Question(text="Which of the following best describes the law of demand?", chapter=demandChapter, topic=demandCurveTopic)
+    
+    questions = [question1, question2, question3, 
+                 question4, question5, question6, 
+                 question7, question8, question9,
+                 question10]
 
+    print("questions created")
+    db.session.add_all(questions)
+    print("questions added")
+    db.session.commit()
+    return questions
+
+def seed_answers(questions):
+    answer1a = Answer(text="The demand for a complementary good will decrease", question=questions[0], is_correct=False)
+    answer1b = Answer(text="The demand for a substitute good will decrease", question=questions[0], is_correct=False)
+    answer1c = Answer(text="No change is exected as a result of the substitution effect", question=questions[0], is_correct=False)
+    answer1d = Answer(text="Quantity demanded increases as the good is now cheaper relative to other substitute goods", question=questions[0], is_correct=True)
+    answer1e = Answer(text="Quantity demanded decreases as the good is now more expensive relative to other substitute goods", question=questions[0], is_correct=False)
+
+    answer2a = Answer(text="Price: $5, Quantity: 10", question=questions[1], is_correct=False)    
+    answer2b = Answer(text="Price: $10, Quantity: 9", question=questions[1], is_correct=False)
+    answer2c = Answer(text="Price: $15, Quantity: 12", question=questions[1], is_correct=True)
+    answer2d = Answer(text="Price: $20, Quantity: 5", question=questions[1], is_correct=False)
+    answer2e = Answer(text="Price: $25, Quantity: 0", question=questions[1], is_correct=False)
+
+    answer3a = Answer(text="It increases from 1 to 14 units", question=questions[2], is_correct=False)
+    answer3b = Answer(text="It increases from 10 to 14 units", question=questions[2], is_correct=False)
+    answer3c = Answer(text="It increases from 4 to 6 units", question=questions[2], is_correct=False)
+    answer3d = Answer(text="It increases from 4 to 8 units", question=questions[2], is_correct=False)
+    answer3e = Answer(text="It increases from 4 to 10 units", question=questions[2], is_correct=True)
+ 
+    answer4a = Answer(text="Consumers' purchasing power", question=questions[3], is_correct=True)
+    answer4b = Answer(text="Demand for a complement good", question=questions[3], is_correct=False)
+    answer4c = Answer(text="Quantity supplied of the good", question=questions[3], is_correct=False)
+    answer4d = Answer(text="Inflation", question=questions[3], is_correct=False)
+    answer4e = Answer(text="The marginal benefit", question=questions[3], is_correct=False)
+
+    answer5a = Answer(text="The income effect and substitution effect cause quantity demanded to move in the same direction", question=questions[4], is_correct=True)
+    answer5b = Answer(text="The substitution effect moves the quanitty demanded in the opposite direction than the substitution effect", question=questions[4], is_correct=False)
+    answer5c = Answer(text="The substitution effect is dominated by the income effect", question=questions[4], is_correct=False)
+    answer5d = Answer(text="The income effect is dominated by the substitution effect", question=questions[4], is_correct=False)
+    answer5e = Answer(text="With an increase in income, the consumer decreases consumption of the good", question=questions[4], is_correct=False)
+
+    answer6a = Answer(text="The total quantity of a good that consumers want to buy at a specific point in time", question=questions[5], is_correct=False)
+    answer6b = Answer(text="The quantity of a good people would buy if they had zero budget constraint", question=questions[5], is_correct=False)
+    answer6c = Answer(text="The total quantity of a good a person or people will buy at different prices", question=questions[5], is_correct=True)
+    answer6d = Answer(text="How badly people want to buy a particular product or service", question=questions[5], is_correct=False)
+    answer6e = Answer(text="The total quantity that firms will sell at a given time", question=questions[5], is_correct=False)
+
+    answer7a = Answer(text="Substitution Effect: Increase, Income Effect: No Change", question=questions[6], is_correct=False)
+    answer7b = Answer(text="Substitution Effect: Increase, Income Effect, Decrease ", question=questions[6], is_correct=False)
+    answer7c = Answer(text="Substitution Effect: Decrease, Income Effect, No Change", question=questions[6], is_correct=False)
+    answer7d = Answer(text="Substitution Effect: Decrease, Income Effect, Decrease", question=questions[6], is_correct=True)
+    answer7e = Answer(text="Substitution Effect: Decrease, Income Effecxt: Increase", question=questions[6], is_correct=False)
+
+    answer8a = Answer(text="Price: 2, Quantity: 12", question=questions[7], is_correct=False)
+    answer8b = Answer(text="Price: 4, Quantity: 6", question=questions[7], is_correct=False)
+    answer8c = Answer(text="Price: 6, Quantity: 4", question=questions[7], is_correct=True)
+    answer8d = Answer(text="Price: 9, Quantity: 2", question=questions[7], is_correct=False)
+    answer8e = Answer(text="Price: 10, Quantity: 5", question=questions[7], is_correct=False)
+
+    answer9a = Answer(text="The demand curve for peanut butter getting steeper", question=questions[8], is_correct=False)
+    answer9b = Answer(text="The demand curve for peanut butter getting flatter", question=questions[8], is_correct=False)
+    answer9c = Answer(text="a decrease in the demand for almond butter, a substitute good.", question=questions[8], is_correct=False)
+    answer9d = Answer(text="a decrease in the quanitty of peanut butter demanded", question=questions[8], is_correct=True)
+    answer9e = Answer(text="an increase in the quanitty of peanut butter supplied", question=questions[8], is_correct=False)
+
+    answer10a = Answer(text="When the price of a good falls, the quantity demanded increases", question=questions[9], is_correct=True)
+    answer10b = Answer(text="Demand is met buy Supply", question=questions[9], is_correct=False)
+    answer10c = Answer(text="When the price of a good rises, its demand decreases", question=questions[9], is_correct=False)
+    answer10d = Answer(text="The price of a good increases when the supply of the good increases", question=questions[9], is_correct=False)
+    answer10e = Answer(text="The price of a good decreases when the demand for the good decreases", question=questions[9], is_correct=False)
+
+    answers = [answer1a, answer1b, answer1c, answer1e, answer1d,
+               answer2a, answer2b, answer2c, answer2e, answer2d,
+               answer3a, answer3b, answer3c, answer3d, answer3e,
+               answer4a, answer4b, answer4c, answer4d, answer4e,
+               answer5a, answer5b, answer5c, answer5d, answer5e,
+               answer6a, answer6b, answer6c, answer6d, answer6e,
+               answer7a, answer7b, answer7c, answer7d, answer7e,
+               answer8a, answer8b, answer8c, answer8d, answer8e,
+               answer9a, answer9b, answer9c, answer9d, answer9e,
+               answer10a, answer10b, answer10c, answer10d, answer10e]
+
+    db.session.add_all(answers)
+    db.session.commit()
 
 def main():
     with app.app_context():
-        pass
+        unit2_questions = seed_unit2_questions()
+        seed_answers(unit2_questions)
+
 
 if __name__=="__main__":
     try:
