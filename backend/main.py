87e22819cd1ba056db4c6c8ada2021f8e348e96c
CharLee Racine <charleeracine@yahoo.com>
2024-11-22 13:16:57 -0700

    update main

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,21 @@
 from flask import Flask, Response, abort, redirect, render_template, request, url_for
 from flask_restx import Api
-from models import User
-from exts import db
+from backend.models import User
+from backend.exts import db
 from flask_jwt_extended import JWTManager
-from auth import auth_ns
-from questions import questions_ns
-from csrf import csrf_ns
+from backend.auth import auth_ns
+from backend.questions import questions_ns
+from backend.csrf import csrf_ns
 from flask_wtf.csrf import CSRFProtect
 from flask_cors import CORS
 from flask_migrate import Migrate
-from progress import progress_ns
-from quiz import quiz_ns
-from results import results_ns
-from units import units_ns
-from chapters import chapters_ns
-from topics import topics_ns
-from retrievedata import retrieve_ns
+from backend.progress import progress_ns
+from backend.quiz import quiz_ns
+from backend.results import results_ns
+from backend.units import units_ns
+from backend.chapters import chapters_ns
+from backend.topics import topics_ns
+from backend.retrievedata import retrieve_ns
 from dotenv import load_dotenv
 import os
 
