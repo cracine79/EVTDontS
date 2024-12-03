08c28737a522b43a8b8a64dfe49045f35d4c0ace
CharLee Racine <charleeracine@yahoo.com>
2024-12-03 09:53:40 -0700

    add user migration

--- backend/migrations/versions/3241d82431ff_added_unit_model_and_many_to_many_.py
+++ backend/migrations/versions/3241d82431ff_added_unit_model_and_many_to_many_.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '3241d82431ff'
-down_revision = None
+down_revision = '12032024'
 branch_labels = None
 depends_on = None
 
