9842354b9075a637a5ee0dead5d3a2504a6c5a8c
CharLee Racine <charleeracine@yahoo.com>
2024-12-04 07:55:49 -0700

    debug more

--- backend/main.py
+++ backend/main.py
@@ -79,7 +79,7 @@ def create_app(config):
                 return send_from_directory(static_dir, 'index.html')
             
     if app.config["ENV"] == "production":
-        print("Registering serve_frontend routes for production environment")
+        print("Registering serve_frontend routes for production environment", app.url_map)
         serve_frontend(app)
 
     return app
