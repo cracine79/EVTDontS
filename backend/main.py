7c0b905c1ffdce49bad4eb80f388c828c87010aa
CharLee Racine <charleeracine@yahoo.com>
2024-12-03 21:36:14 -0700

    try adding route to /

--- backend/main.py
+++ backend/main.py
@@ -61,9 +61,7 @@ def create_app(config):
         print(f"App root path: {app.root_path}")
 
         
-        @app.route('/', defaults={'path':''}) 
-        def servio():
-            return 'Home homie'
+        @app.route('/', defaults={'path':'/'}) 
         @app.route('/<path:path>')
         def serve(path):
             print(f"Path: {path}")
