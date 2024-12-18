d4c9ab2da409db057aaf6a26946ca980567317c5
CharLee Racine <charleeracine@yahoo.com>
2024-12-18 12:34:50 -0700

    search upon form submission

--- frontend2/src/Components/SearchBar.jsx
+++ frontend2/src/Components/SearchBar.jsx
@@ -13,13 +13,18 @@ export const SearchBar = () =>{
     }
     return (
         <div className="flex justify-center h-8 w-full">
+            <form onSubmit={(e)=>{
+                e.preventDefault()
+                findIt()}} className="flex justify-center h-8 w-full">
             <input className='lg:w-5/6 md:w-2/3 w-1/2 rounded-tl pl-4 rounded-bl border-t border-black
                             border-l border-b'   
                     placeholder='Search for Economics Videos Here' 
                     onChange={(e)=>setSearchTerm(e.target.value)}></input>
-            <button className='bg-slate-400 hover:bg-slate-500 hover:cursor-pointer w-1/12 min-w-12 rounded-tr rounded-br
+            <button type='submit' className='bg-slate-400 hover:bg-slate-500 hover:cursor-pointer w-1/12 min-w-12 rounded-tr rounded-br
                                 border-black border-r border-t border-b'
-                    onClick = {()=>findIt()}>Get It!</button>
+                   >Get It!</button>
+            </form>
+       
         </div>
     )
 }
