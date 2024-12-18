484192f5134a24d1a415248a9dcd773619d8f43a
CharLee Racine <charleeracine@yahoo.com>
2024-12-18 12:26:49 -0700

    stylize results page add no results image text

--- frontend2/src/Components/SearchResults.jsx
+++ frontend2/src/Components/SearchResults.jsx
@@ -13,15 +13,19 @@ export const SearchResults = () => {
     const goToAllVids = (videoUrl) => {
         navigate('/videoindex', {state:{videoUrl:videoUrl}})
     }
+
+    const goToThisVid = ( (chapterId) => {
+        navigate(`/video/${chapterId}`)
+    })
     return(
         <div className='mt-32'>
-            <div className='w-full flex items-center flex-col justify-center'>
+            <div className='w-full flex items-center flex-col  min-h-[65vh]'>
                 <div className='text-2xl mb-8'>Search Results For "{search_term}"</div>
                 <div className='w-full flex flex-wrap justify-around'> 
                     {
                         searchObj.map((chapter)=>{
-                            return(<div key={chapter.id} className='w-1/4 min-w-72 border border-black flex flex-col h-auto items-center m-4 '>
-                                <div className='mt-2 font-bold'>
+                            return(<div key={chapter.id} className='w-1/4 min-w-72 border border-slate-200 px-2 shadow-xl flex flex-col h-auto items-center m-4 rounded '>
+                                <div className='my-4 text-lg text-center font-bold'>
                                 {chapter.name}
                                 </div>
                                 <div className="w-full min-w-72 relative" style={{paddingBottom: '56.25%'}}> {/* Aspect Ratio 16:9 */}
@@ -41,10 +45,26 @@ export const SearchResults = () => {
                                 </button>
 
                                 }
+                                { user.username && 
+                                    
+                                    <button className="border-black border hover:cursor-pointer bg-slate-300 hover:bg-slate-500 rounded my-4"
+                                            onClick = {()=>goToThisVid(chapter.id)}>
+                                        Jump to this Video
+                                    </button>
+    
+                                    }
                             </div>)
                         })
                     }
                 </div>
+  
+                {
+                    searchObj.length == 0 && <div className='w-1/3'>
+                       <div className='text-xl mb-2 font-bold text-center'>Whoops!  Looks like we don't have a video for that search yet!</div>  
+                       <img src='https://evtds-seeds.s3.us-east-2.amazonaws.com/IslandNight.png'></img>
+                        <div className='mt-8 my-8'>We blame the economics gods for not blessing us with one yet. But hey, if you reach out to us (and it's actually about economics, not like that time you searched for "cooking with Keynes"), we'll see what we can do. No promises, though... the gods are a tough crowd.</div>
+                    </div>
+                }
             </div>
         </div>
     )
