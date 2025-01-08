bdbad07e07e1625f91a17c117313e0cd4dbae2d4
CharLee Racine <charleeracine@yahoo.com>
2025-01-07 21:23:23 -0700

    more tax qs

--- backend/seed_questions_taxes.py
+++ backend/seed_questions_taxes.py
@@ -13,7 +13,7 @@ def seed_tax_chapter():
         print("Unit with ID 4 does not exist. Seeding aborted.")
         return
 
-    chapter4_1 = Chapter(name="4.1 Taxes", unit=unit4, video_url="https://www.youtube.com", order=25)
+    chapter4_1 = Chapter(name="4.1 Taxes", unit=unit4, video_url="https://www.youtube.com/embed/4Jpvus1kpr8", order=25)
 
     topic34 = QuestionTopic(name='Taxes - Impact on price and quantity', chapter=chapter4_1)
     topic35 = QuestionTopic(name='Taxes - Impact on Producer Revenue, Consumer Expenditure & Government Revenue', chapter=chapter4_1)
@@ -26,10 +26,32 @@ def seed_tax_chapter():
     question2 = Question(text='The diagram above shows the effect of a unit tax placed on a good.  What is the price paid by consumers and price received by producers after the tax is paid?', chapter=chapter4_1, topic=topic34, image_url = "https://evtds-seeds.s3.us-east-2.amazonaws.com/TaxCalcDiagram.png")
     question3 = Question(text='The diagram above shows the effect of a unit tax placed on a good.  According to the diagram, what is the dollar amount of the unit tax?', chapter=chapter4_1, topic=topic34, image_url = "https://evtds-seeds.s3.us-east-2.amazonaws.com/TaxCalcDiagram.png")
     question4 = Question(text='Assume that the government increases the unit exise tax on alcohol suppliers and also at the same time, the demand for alcohol increases due to increases in stress levels at work.   As a result, equilibrium price and quantity of alcohol will most likely change in which of the following ways?', chapter=chapter4_1, topic=topic34)
-    
+    question5 = Question(text="Assume supply of electric vehicles is upward sloping and demand is downward sloping.  An imposition of a specific tax on suppliers of electric vehicles would result in which of the following?", chapter=chapter4_1, topic=topic34)
+
     question1burden = Question(text='If market demand for a good is elastic and supply is inelastic, which of the following is true when there is an increase in sales tax?', chapter=chapter4_1, topic=topic37)
     question2burden = Question(text="Assume that price elasticity of demand for good A is -0.3 and price elasticity of demand for good B is -2.5.  Assume goods A and B have identical demand curves.  If a per-unit excise tax of the same amount is imposed on both goods, which of the following is true?", topic=topic37, chapter=chapter4_1)
-    questions = [question1, question2, question3, question4, question1burden, question2burden]
+    question3burden = Question(text="Assume that supply of wheat is relatively price inelastic while demand for wheat is relatively price elastic.  If the government imposes a specific exise tax on the production of corn, the incidence of tax will fall")
+    question4burden = Question(text="Assume demand for cigarettes is inelastic and supply of cigarettes is elastic. If the government applies an exise tax on cigarettes, which of the following will occur in the short run?", topic = topic37, chapter=chapter4_1)
+    question5burden = Question(text="Assume that the government imposes a specific excise tax on the producers of a good that faces perfectly inelastic demand.  After the tax, the price and the quantity will change in which of the following ways?", topic = topic37, chapter=chapter4_1)
+    
+    question1rev = Question(text="If the demand for a good is downward sloping and supply is upward sloping, imposing a sales tax on the good will", topic=topic35, chapter=chapter4_1)
+    question2rev = Question(text="In the graph above, an excise tax has been imposed, moving supply from Supply to Supply + Tax.  The area representing the tax revenue to the government is", topic=topic35, chapter=chapter4_1, image_url = "https://evtds-seeds.s3.us-east-2.amazonaws.com/TaxRevCalcDiagram_1.png")
+    question3rev = Question(text="In the graph above, an excise tax has been imposed, moving supply from Supply to Supply + Tax.  The area representing total consumer expenditure after imposition of the tax is", topic=topic35, chapter=chapter4_1, image_url = "https://evtds-seeds.s3.us-east-2.amazonaws.com/TaxRevCalcDiagram_1.png")
+    question4rev = Question(text="In the graph above, an excise tax has been imposed, moving supply from Supply to Supply + Tax.  The area representing total producer revenue after imposition of the tax is", topic=topic35, chapter=chapter4_1, image_url = "https://evtds-seeds.s3.us-east-2.amazonaws.com/TaxRevCalcDiagram_1.png")
+    
+    question1s = Question(text='Suppose that the market supply for pants is upward sloping and the market demand for pants is downward sloping.  How will the imposition of a sales tax on pants affect the consumer surplus (CS), producer surplus (PS), and total surplus (TS)?', chapter=chapter4_1, topic=topic36)
+    question2s = Question(text='In the graph above, an excise tax has been imposed, moving supply from Supply to Supply + Tax.  The area representing total Loss of Social Surplus/Deadweight Loss is represented by:', chapter=chapter4_1, topic=topic36, image_url = "https://evtds-seeds.s3.us-east-2.amazonaws.com/TaxRevCalcDiagram_1.png")
+    question3s = Question(text='If the government imposes a per-unit tax on airline travel, consumer surplus in the airline travel market will', chapter=chapter4_1, topic=topic36)
+    question4s = Question(text='If the government imposes a per-unit tax on airline travel, producer surplus in the airline travel market will', chapter=chapter4_1, topic=topic36)
+    question5s = Question(text='Assume that the government imposes a $5 per-unit tax on the sellers of a good in the market depicted in the diagram above.  What are the price paid by consumers, price received by producers, and deadweight loss?', chapter=chapter4_1, topic=topic36, image_url = "https://evtds-seeds.s3.us-east-2.amazonaws.com/5DollarTaxCalc_1.png")
+    
+    questions = [question1, question2, question3, question4, question5, 
+                 question1burden, question2burden, question3burden,
+                 question4burden, question5burden,
+                 question1rev, question2rev, question3rev,
+                 question4rev,
+                 question1s, question2s, question3s,
+                 question4s, question5s, ]
 
     answer1a = Answer(text="Price: Increase, Quantity: Increase", question=question1, is_correct=False)
     answer1b = Answer(text="Price: Increase, Quantity: Decrease", question=question1, is_correct=True)
@@ -55,8 +77,14 @@ def seed_tax_chapter():
     answer4d = Answer(text="Price: Increase, Quantity: Indeterminate", question=question4, is_correct=True)
     answer4e = Answer(text="Price: Indeterminate, Quantity: Increase", question=question4, is_correct=False)
 
+    answer5a = Answer(text="The price recieved by producers of electric vehicles will increase", question=question5, is_correct=False)
+    answer5b = Answer(text="The price paid by consumers of electric vehicles will decrease", question=question5, is_correct=False)
+    answer5c = Answer(text="The quantity purchased of electric vehicles will increase", question=question5, is_correct=False)
+    answer5d = Answer(text="The quantity sold of electric vehicles will decrease", question=question5, is_correct=True)
+    answer5e = Answer(text="The total revenue of electric vehicle manufacturers will increase", question=question5, is_correct=False)
+
     answer1ba = Answer(text="Producers will bear more of the burden of the tax", question=question1burden, is_correct=False)
-    answer1bb = Answer(text="Consumers will bear more of the burden of the tax", question=question1burden, is_correct=False)
+    answer1bb = Answer(text="Consumers will bear more of the burden of the tax", question=question1burden, is_correct=True)
     answer1bc = Answer(text="Producers will bear all of the burden of the tax", question=question1burden, is_correct=False)
     answer1bd = Answer(text="Consumers will bear all of the burden of the tax", question=question1burden, is_correct=False)
     answer1be = Answer(text="The burden of the tax will be split evenly between producers and consumers", question=question1burden, is_correct=False)
@@ -67,13 +95,104 @@ def seed_tax_chapter():
     answer2bd = Answer(text="The tax burden paid by producers of good B would be relatively lower than that paid by producers of good A", question=question2burden, is_correct=False)
     answer2be = Answer(text="The tax share paid by suppliers of goods A and B would be the same", question=question2burden, is_correct=False)
     
+    answer3ba = Answer(text="completely on consumers", question=question3burden, is_correct=False)
+    answer3bb = Answer(text="more on consumers than producers", question=question3burden, is_correct=False)
+    answer3bc = Answer(text="completely on producers", question=question3burden, is_correct=False)
+    answer3bd = Answer(text="more on producers than consumers", question=question3burden, is_correct=True)
+    answer3be = Answer(text="equally on consumers and producers", question=question3burden, is_correct=False)
+
+    answer4ba = Answer(text = "Tax burden will fall equally on producers and consumers", is_correct=False, question=question4burden)
+    answer4bb = Answer(text = "Tax burden will fall more on consumers", is_correct=True, question=question4burden)
+    answer4bc = Answer(text = "Tax burden will fall more on producers", is_correct=False, question=question4burden)
+    answer4bd = Answer(text = "Percentage increase in price of cigarettes will exceed the percentage increase in quantity demanded", is_correct=False, question=question4burden)
+    answer4be = Answer(text = "Percentage decrease in total revenue will exceed the percentage decrease in quantity demanded", is_correct=False, question=question4burden)
+    
+    answer5ba = Answer(text="Price: Decrease, Quantity: No Change", question=question5burden, is_correct=False)
+    answer5bb = Answer(text="Price: No Change, Quantity: Increase", question=question5burden, is_correct=False)
+    answer5bc = Answer(text="Price: Increase, Quantity: No Change", question=question5burden, is_correct=True)
+    answer5bd = Answer(text="Price: Increase, Quantity: Increase", question=question5burden, is_correct=False)
+    answer5be = Answer(text="Price: Increase, Quantity: Decrease", question=question5burden, is_correct=False)
+
+    answer1sa = Answer(text="CS: Decrease, PS: Decrease, TS: Decrease", question=question1s, is_correct=True)
+    answer1sb = Answer(text="CS: Decrease, PS: Increase, TS: Increase", question=question1s, is_correct=False)
+    answer1sc = Answer(text="CS: Decrease, PS: Increase, TS: Decrease", question=question1s, is_correct=False)
+    answer1sd = Answer(text="CS: Increase, PS: Decrease, TS: Decrease", question=question1s, is_correct=False)
+    answer1se = Answer(text="CS: Increase, PS: Increase, TS: Increase", question=question1s, is_correct=False)
+
+    answer2sa = Answer(text="C, F, G", question=question2s, is_correct=True)
+    answer2sb = Answer(text="C, F, B", question=question2s, is_correct=False)
+    answer2sc = Answer(text="C, E, F", question=question2s, is_correct=False)
+    answer2sd = Answer(text="E, F, G", question=question2s, is_correct=False)
+    answer2se = Answer(text="A, B, F, D", question=question2s, is_correct=False)
+
+    answer3sa = Answer(text="Increase or decrease, depending on price elastiicty of demand", question=question3s, is_correct=False)
+    answer3sb = Answer(text="Increase, because consumers will buy more airline travel", question=question3s, is_correct=False)
+    answer3sc = Answer(text="Increase, because airline companies will pass the tax on to consumers", question=question3s, is_correct=False)
+    answer3sd = Answer(text="Decrease, because there will be more airline travel available", question=question3s, is_correct=False)
+    answer3se = Answer(text="Decrease, because price paid for airline travel will increase, and quantity consumed will decrease", question=question3s, is_correct=True)
+
+    answer4sa = Answer(text="Increase or decrease, depending on price elastiicty of supply", question=question3s, is_correct=False)
+    answer4sb = Answer(text="Increase, because airline companies will sell more airline travel", question=question3s, is_correct=False)
+    answer4sc = Answer(text="Increase, because airline companies will pass the tax on to consumers", question=question3s, is_correct=False)
+    answer4sd = Answer(text="Decrease, because there will be more airline travel available", question=question3s, is_correct=False)
+    answer4se = Answer(text="Decrease, because price received by airline companies for supplying airline travel will decrease, and quantity supplied will decrease", question=question3s, is_correct=True)
+
+    answer5sa = Answer(text="Buyer Price Paid: $14, Seller Price Received: $10, Deadweight Loss: $50", question=question5s, is_correct=False)
+    answer5sb = Answer(text="Buyer Price Paid: $14, Seller Price Received: $9, Deadweight Loss: $125", question=question5s, is_correct=True)
+    answer5sc = Answer(text="Buyer Price Paid: $10, Seller Price Received: $10, Deadweight Loss: $0", question=question5s, is_correct=False)
+    answer5sd = Answer(text="Buyer Price Paid: $9, Seller Price Received: $14, Deadweight Loss: $50", question=question5s, is_correct=False)
+    answer5se = Answer(text="Buyer Price Paid: $9, Seller Price Received: $14, Deadweight Loss: $125", question=question5s, is_correct=False)
+
+    answer1ra = Answer(text="Result in no change to price paid by consumers", question=question1rev, is_correct=False)
+    answer1rb = Answer(text="Result in no change to after tax revenue recieved by producers", question=question1rev, is_correct=False)
+    answer1rc = Answer(text="Increase after tax revenue received by producers", question=question1rev, is_correct=False)
+    answer1rd = Answer(text="Decrease after tax revenue recieved by producers", question=question1rev, is_correct=True)
+    answer1re = Answer(text="Not change total exp", question=question1rev, is_correct=False)
+
+    answer2ra = Answer(text="P1, B, F, P3", question=question2rev, is_correct = False)
+    answer2rb = Answer(text="P1, B, Q3, 0", question=question2rev, is_correct = False)
+    answer2rc = Answer(text="P2, C, G, P4", question=question2rev, is_correct = True)
+    answer2rd = Answer(text="P2, C, Q2, 0", question=question2rev, is_correct = False)
+    answer2re = Answer(text="P3, F, Q3, 0", question=question2rev, is_correct = False)
+
+    answer3ra = Answer(text="P1, B, Q3, 0", question=question3rev, is_correct=False)
+    answer3rb = Answer(text="P2, C, G, P4", question=question3rev, is_correct=False)
+    answer3rc = Answer(text="P2, C, Q2, 0, ", question=question3rev, is_correct=True)
+    answer3rd = Answer(text="P3, F, Q3, 0", question=question3rev, is_correct=False)
+    answer3re = Answer(text="P4, G, Q2, 0", question=question3rev, is_correct=False)
+
+    answer4ra = Answer(text="P1, B, Q3, 0", question=question4rev, is_correct=False)
+    answer4rb = Answer(text="P2, C, G, P4", question=question4rev, is_correct=False)
+    answer4rc = Answer(text="P2, C, Q2, 0, ", question=question4rev, is_correct=False)
+    answer4rd = Answer(text="P3, F, Q3, 0", question=question4rev, is_correct=False)
+    answer4re = Answer(text="P4, G, Q2, 0", question=question4rev, is_correct=True)
+
+
     answers = [
         answer1a, answer1b, answer1c, answer1d, answer1e, 
         answer2a, answer2b, answer2c, answer2d, answer2e, 
         answer3a, answer3b, answer3c, answer3d, answer3e, 
         answer4a, answer4b, answer4c, answer4d, answer4e, 
+        answer5a, answer5b, answer5c, answer5d, answer5e,
+
         answer1ba, answer1bb, answer1bc, answer1bd, answer1be, 
-        answer2ba, answer2bb, answer2bc, answer2bd, answer2be
+        answer2ba, answer2bb, answer2bc, answer2bd, answer2be,
+        answer3ba, answer3bb, answer3bc, answer3bd, answer3be,
+        answer5ba, answer5bb, answer5bc, answer5bd, answer5be,
+        answer4ba, answer4bb, answer4bc, answer4bd, answer4be,
+
+        answer1sa, answer1sb, answer1sc, answer1sd, answer1se, 
+        answer2sa, answer2sb, answer2sc, answer2sd, answer2se,
+        answer3sa, answer3sb, answer3sc, answer3sd, answer3se,
+        answer4sa, answer4sb, answer4sc, answer4sd, answer4se,
+        answer5sa, answer5sb, answer5sc, answer5sd, answer5se,
+
+        answer1ra, answer1rb, answer1rc, answer1rd, answer1re,
+        answer2ra, answer2rb, answer2rc, answer2rd, answer2re,
+        answer3ra, answer3rb, answer3rc, answer3rd, answer3re,
+        answer4ra, answer4rb, answer4rc, answer4rd, answer4re
+        
+        
     ]
 
     try:
