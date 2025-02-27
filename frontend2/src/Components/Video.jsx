0d231d0efc183ddf96718799426db04d0e2f497b
CharLee Racine <charleeracine@yahoo.com>
2025-02-27 13:47:54 -0700

    change color of userless results prompt

--- frontend2/src/Components/Video.jsx
+++ frontend2/src/Components/Video.jsx
@@ -37,24 +37,6 @@ export const Video = () => {
         navigate('/userhome')
     }
 
-    // const updateVideoProgress = async(chapterId) => {
-    //     try {
-    //         const response = await csrfFetch('/api/progress/' , {
-    //             method: 'PATCH',
-    //             headers: {
-    //                 'Content-Type': 'application/json',
-    //               },
-    //             body: JSON.stringify({ chapter_id: chapterId }),
-    //         });
-
-    //     const data = await response.json()
-
-    //     dispatch(updateUserChapters(data))
-    //     } catch (error) {
-    //         console.error("error finding", error)
-    //     }
-    // }
-
     const completeVid = () => {
         if(!alreadyWatched){
             dispatch(updateVideoProgress(chapterId, "watched"))
@@ -62,8 +44,6 @@ export const Video = () => {
         navigate('/quiz', {state: {chapter: chapterId, type: 'chapterQuiz', topics:[]}})
     }
     
-
-  
     return(
         <div className='flex flex-col justify-center items-center'>
             <div className="mt-24 sm:mt-40 sm:mb-12 sm:min-h-[65vh] mx-auto px-8 grid grid-cols-1  sm:grid-cols-2  items-start">
@@ -103,38 +83,5 @@ export const Video = () => {
             </div>
         </div>
 
-        
-        // <div className="w-full flex flex-col justify-center items-center mt-48">
-        //      <div className='text-center sm:text-3xl text-xl  my-6'>{currentChapter.name}</div>
-        
-        // <div className='aspect-video justify-between sm:w-1/2 w-full flex flex-col'>
-           
-        //     <div className="w-full h-full">
-        //         <iframe className="w-full h-full" 
-        //                 src={`${source}?modestbranding=1&rel=0&controls=1&autohide=1`}
-        //                 title="title" 
-        //                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-        //                 referrerPolicy="strict-origin-when-cross-origin" 
-        //                 allowFullScreen></iframe>
-        //     </div>
-        // </div>
-
-
-        //     <p className="whitespace-pre-line sm:w-3/4 w-5/6 mt-10 hidden sm:block" dangerouslySetInnerHTML={{ __html: blurb }}></p>
-        //     <div className='flex justify-between sm:flex-row flex-col w-1/2 mt-6 mb-8'>
-        //         <div onClick = {completeVid} className="button  !px-8 !py-2">
-        //                 <p className='hidden sm:inline'>Mark as Completed &&nbsp;</p>
-        //                 <p>Rock the Quiz</p>
-        //         </div>
-        //         <div onClick = {handleClick} className="button !flex !items-center !justify-center !py-2">
-        //             Back to my Dashboard
-        //         </div>
-
-                
-        //     </div>
-        //     <p className="whitespace-pre-line sm:w-3/4 w-5/6 mt-4 mb-6 sm:hidden" dangerouslySetInnerHTML={{ __html: blurb }}>
-        //         {/* {blurb} */}
-        //     </p>
-        // </div>
     )
 }
